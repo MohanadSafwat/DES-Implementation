aa99e0e2a62fc136efe4dd06cc7638feb56fe803
MohanadSafwat <57914175+MohanadSafwat@users.noreply.github.com>
2021-11-10 01:05:02 +0200

    Initial Commit

---
+++ SecurityProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SecurityProject
+//
+//  Created by Mohanad Safwat on 10/11/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
